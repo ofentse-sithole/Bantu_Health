4971c8f6c68f9b830acb4ef3f056bf96d73cfa63
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-10-25 01:31:01 +0200

    app router

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import React, { useEffect } from 'react';
-import { View } from 'react-native';
-import SplashScreenComponent from './pages/SplashScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Register from "./pages/Register.js";
+import Login from "./pages/Login.js/index.js";
+import Dashboard from "./pages/Dashboard.js/index.js";
+import SymptomsAnalysis from "./pages/SymptomsAnalysis.js/index.js";
+import SplashScreen from './pages/SplashScreen';
+import Toast from 'react-native-toast-message';
 
 const App = () => {
   return (
-    <SplashScreenComponent />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{ headerShown: false }} // Hide the header for SplashScreen
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{ headerShown: false }} // Hide the header for Login
+        />
+        <Stack.Screen 
+          name="Register" 
+          component={Register} 
+          options={{ headerShown: false }} // Hide the header for Register
+        />
+        <Stack.Screen 
+          name="Dashboard" 
+          component={Dashboard} 
+          options={{ headerShown: false }} // Hide the header for Dashboard
+        />
+        <Stack.Screen 
+          name="SymptomsAnalysis" 
+          component={SymptomsAnalysis} 
+          options={{ headerShown: false }} // Hide the header for SymptomsAnalysis
+        />
+      </Stack.Navigator>
+
+      {/* Toast component should be placed here */}
+      <Toast />
+    </NavigationContainer>
   );
 };
 
