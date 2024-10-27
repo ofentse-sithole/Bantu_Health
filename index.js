204b6fe7d4d830b1dc523b36eb5c832661856471
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-10-27 11:33:55 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
+import { AppRegistry } from 'react-native';
 import { registerRootComponent } from 'expo';
-
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+AppRegistry.registerComponent('main', () => App);
 registerRootComponent(App);
